8c9e4b544e385af9130ede4518ac308faf37622e
cdany840 <cdanymira@gmail.com>
2024-08-15 19:26:46 -0600

    second commit

--- appsalon_mvc_php.sql
+++ appsalon_mvc_php.sql
@@ -17,6 +17,25 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE TABLE `servicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `precio` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;;
+
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `apellido` varchar(60) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `confirmado` tinyint(1) DEFAULT NULL,
+  `token` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;;
 
 CREATE TABLE `citas` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -39,32 +58,6 @@ CREATE TABLE `citasServicios` (
   CONSTRAINT `citasservicios_ibfk_4` FOREIGN KEY (`servicioId`) REFERENCES `servicios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3;;
 
-CREATE TABLE `servicios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(60) DEFAULT NULL,
-  `precio` decimal(5,2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;;
-
-CREATE TABLE `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(60) DEFAULT NULL,
-  `apellido` varchar(60) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `password` varchar(60) DEFAULT NULL,
-  `telefono` varchar(10) DEFAULT NULL,
-  `admin` tinyint(1) DEFAULT NULL,
-  `confirmado` tinyint(1) DEFAULT NULL,
-  `token` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;;
-
-INSERT INTO `citas` (`id`, `fecha`, `hora`, `usuarioId`) VALUES
-(22, '2023-11-29', '10:30:00', 9);
-
-INSERT INTO `citasServicios` (`id`, `citaId`, `servicioId`) VALUES
-(18, 22, 2);
-
 INSERT INTO `servicios` (`id`, `nombre`, `precio`) VALUES
 (1, 'Corte de Cabello Mujer ACTUALIZADO', 120.00),
 (2, 'Corte de Cabello Hombre', 80.00),
@@ -81,7 +74,11 @@ INSERT INTO `servicios` (`id`, `nombre`, `precio`) VALUES
 INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `admin`, `confirmado`, `token`) VALUES
 (9, 'Daniel', 'Miranda', 'daniel@miranda.com', '$2y$10$9TTiKdZXQaUQaSbVKd7wPOucLusU8ebkv2h2IgqNjQXs.uLTW7CAq', '1234567890', 0, 1, '');
 
+INSERT INTO `citas` (`id`, `fecha`, `hora`, `usuarioId`) VALUES
+(22, '2023-11-29', '10:30:00', 9);
 
+INSERT INTO `citasServicios` (`id`, `citaId`, `servicioId`) VALUES
+(18, 22, 2);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
